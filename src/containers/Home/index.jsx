ecf5e7887c0215e8bdf80580ff635f4b53013164
Robson077 <minhamae830@gmail.com>
2023-06-24 13:41:19 -0300

    feat: add useState and useEffect

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,20 +1,34 @@
+import { useState, useEffect } from "react"
+
 import api from "../../services/api"
 import { Background } from "./styles"
 
 function Home() {
-    async function getMovies() {
-        const data = await api.get("/movie/popular")
+    const [movie, setMovie] = useState()
 
-        console.log(data)
-    }
+    useEffect(() => {
+        async function getMovies() {
+            const {
+                data: { results }
+            } = await api.get("/movie/popular")
 
-    getMovies()
+            setMovie(results[14])
+        }
+        console.log(movie)
+        getMovies()
+    }, [])
 
     return (
-        <Background img="https://image.tmdb.org/t/p/original//1inZm0xxXrpRfN0LxwE2TXzyLN6.jpg">
-            <h1>Home</h1>
-            <p>Essa é a home</p>
-        </Background>
+        <>
+            {movie && (
+                <Background
+                    img={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                >
+                    <h1>{movie.title}</h1>
+                    <p>{movie.overview}</p>
+                </Background>
+            )}
+        </>
     )
 }
 
