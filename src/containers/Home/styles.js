667abe814386297f8564c95719ac12142692e95e
Robson077 <minhamae830@gmail.com>
2023-06-24 14:09:07 -0300

    feat: add poster and container

--- src/containers/Home/styles.js
+++ src/containers/Home/styles.js
@@ -20,3 +20,41 @@ export const Background = styled.div`
         background-color: rgba(0, 0, 0, 0.5);
     }
 `
+
+export const Info = styled.div`
+    z-index: 2;
+    padding: 20px;
+    width: 50%;
+
+    h1 {
+        font-size: 3rem;
+        font-weight: 700;
+        color: #ffffff;
+    }
+
+    P {
+        font-size: 20px;
+        font-weight: 500;
+        color: #ffffff;
+        margin-top: 30px;
+        margin-bottom: 20px;
+    }
+`
+
+export const Poster = styled.div`
+    z-index: 2;
+
+    img {
+        width: 300px;
+        border-radius: 30px;
+    }
+`
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    height: 100%;
+    max-width: 1500px;
+`
