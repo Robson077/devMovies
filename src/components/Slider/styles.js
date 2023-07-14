cbfb022466d6861dc79cce219047ceee8eaec505
Robson077 <minhamae830@gmail.com>
2023-07-14 20:00:20 -0300

    feat: arrumando codigo

--- src/components/Slider/styles.js
+++ src/components/Slider/styles.js
@@ -1,3 +1,18 @@
 import styled from "styled-components"
 
-export const Container = styled.div``
+export const Container = styled.div`
+    background: #000000;
+    padding: 0 20px;
+    overflow: hidden;
+
+    h2 {
+        color: #ffffff;
+        font-size: 25px;
+        margin: 50px 0 20px 20px;
+    }
+
+    .swiper-wrapper {
+        display: flex;
+        flex-direction: row;
+    }
+`
