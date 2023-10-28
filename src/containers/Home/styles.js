70120f0442289e58a590d037d32f982fb049bc65
Robson Santana <minhamae830@gmail.com>
2023-10-28 09:59:55 -0300

    styles: adicionando alguns estilos

--- src/containers/Home/styles.js
+++ src/containers/Home/styles.js
@@ -1,4 +1,13 @@
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
+
+const scale = keyframes`
+    from {
+        transform: scale(0);
+    }
+    to {
+        transform: scale(1);
+    }
+`
 
 export const Background = styled.div`
     background-image: url(${(props) => props.img});
@@ -9,6 +18,7 @@ export const Background = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    position: relative;
 
     &::before {
         content: "";
@@ -18,6 +28,16 @@ export const Background = styled.div`
         width: 100%;
         height: 100%;
         background-color: rgba(0, 0, 0, 0.5);
+    }
+
+    &::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 150px;
+        background-image: linear-gradient(to top, #000, rgba(0, 0, 0, 0));
     }
 `
 
@@ -47,6 +67,7 @@ export const Poster = styled.div`
     img {
         width: 300px;
         border-radius: 30px;
+        animation: ${scale} 0.5s linear;
     }
 `
 
